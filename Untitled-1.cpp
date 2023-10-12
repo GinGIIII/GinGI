836ba06e74ed83cc6187d4d52091186201e12946
GinGIIII <146040392+GinGIIII@users.noreply.github.com>
2023-10-12 23:44:17 +1000

    UmpaLumpa.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <cmath> // Добавляем заголовочный файл для sqrt
+#include <cmath>
 
 int main() {
-    // Инициализация генератора случайных чисел
-    srand(time(0));
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
 
     int totalPoints = 0;
 
-    // Цикл для 5 выстрелов
     for (int i = 0; i < 5; ++i) {
-        double xi, yi;
-        std::cout << "Введите координаты xi и yi для выстрела " << i + 1 << ": ";
+        int xi, yi;
+        std::cout << "Введите координаты для выстрела " << i + 1 << " (x y): ";
         std::cin >> xi >> yi;
 
-        // Генерация случайных отклонений от попадания
-        int random_xi = rand() % 11 - 5; // От -5 до 5
-        int random_yi = rand() % 11 - 5;
+        // Добавим случайное отклонение от -5 до 5
+        int random_xi = std::rand() % 11 - 5;
+        int random_yi = std::rand() % 11 - 5;
 
-        // Вычисление расстояния от центра мишени
-        double distance = sqrt(xi * xi + yi * yi);
+        // Вычислим радиус точки
+        double radius = std::sqrt((xi + random_xi) * (xi + random_xi) + (yi + random_yi) * (yi + random_yi));
 
-        // Оценка баллов в зависимости от расстояния
+        // Вычислим баллы за выстрел
         int points = 0;
-        if (distance < 1.0) {
+        if (radius >= 0 && radius < 1) {
             points = 5;
-        } else if (distance < 2.0) {
+        } else if (radius >= 1 && radius < 2) {
             points = 4;
-        } else if (distance < 3.0) {
+        } else if (radius >= 2 && radius < 3) {
             points = 3;
-        } else if (distance < 4.0) {
+        } else if (radius >= 3 && radius < 4) {
             points = 2;
-        } else if (distance < 5.0) {
+        } else if (radius >= 4 && radius < 5) {
             points = 1;
+        } else {
+            // Если вы попали вне радиуса, начислить 0 баллов
+            points = 0;
         }
-
-        // Суммирование баллов
+        
+        
         totalPoints += points;
-
-        // Вывод результата выстрела
-        std::cout << "Попадание в (" << xi << ", " << yi << ") со случайными отклонениями (" << random_xi << ", " << random_yi << "). Заработано " << points << " баллов." << std::endl;
+        // Вывод случайного отклонения
+        std::cout << "Случайное отклонение (random_x, random_y): " << random_xi << ", " << random_yi << std::endl;
+        std::cout << "За выстрел " << i + 1 << " получено " << points << " баллов. Радиус точки: " << radius << ". Всего баллов: " << totalPoints << std::endl;
     }
-
-    // Вывод общего числа баллов и статуса "лузер"
-    std::cout << "Сумма баллов: " << totalPoints << std::endl;
+    // Проверим, является ли игрок "лузером"
     if (totalPoints < 10) {
-        std::cout << "Лузер" << std::endl;
-    }
-
-    return 0;
+    std::cout << "Сумма баллов: " << totalPoints << " лузер" << std::endl;
+} else {
+    std::cout << "Сумма баллов: " << totalPoints << " чеееел, мега крут!" << std::endl;
+}
+return 0;
 }
